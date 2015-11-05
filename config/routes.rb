3492d2522488cdf522b9a47b2dc41fbebfd08329
elzzup <guild0105@gmail.com>
2015-11-06 06:33:59 +0900

    Create twitter log

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 end
