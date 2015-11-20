198ff9fb589bd8addad926a2739c7022f1979466
elzzup <guild0105@gmail.com>
2015-11-21 03:21:42 +0900

    Use localstrage for safe editor

--- app/assets/javascripts/article_form.js
+++ app/assets/javascripts/article_form.js
@@ -1,12 +1,28 @@
 var marked = require('marked');
-console.log(marked);
 
 $(function() {
+    var $title = $('#article_title');
     var $text_edit = $('#article_text');
     var $text_preview = $('#text-preview-area');
+    var l = localStorage;
+
+    // ローカルストレージに保存されていたら復元
+    if (l.getItem("text") != null) {
+        var text = l.getItem("text");
+        $text_edit.val(text);
+        $text_preview.html(marked(text));
+    }
+    if (l.getItem("title") != null) {
+        $title.val(l.getItem("title"));
+    }
 
     // 変更をマークダウンにしプレビューに反映する
     $text_edit.keyup(function() {
         $text_preview.html(marked($(this).val()));
+        l.setItem("text", $(this).val());
     });
+    $title.keyup(function() {
+        l.setItem("title", $(this).val());
+    });
+
 });
