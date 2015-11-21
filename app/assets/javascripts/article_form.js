895c4dd91d885ccdf155c7872e7bb767ead13928
elzzup <guild0105@gmail.com>
2015-11-21 17:34:42 +0900

    Fix script localstrages

--- app/assets/javascripts/article_form.js
+++ app/assets/javascripts/article_form.js
@@ -1,19 +1,24 @@
 var marked = require('marked');
 
 $(function() {
+    console.log('loaded article_form');
     var $title = $('#article_title');
     var $text_edit = $('#article_text');
     var $text_preview = $('#text-preview-area');
+    var $submit_btn = $('#btn-submit');
     var l = localStorage;
 
     // ローカルストレージに保存されていたら復元
     if (l.getItem("text") != null) {
         var text = l.getItem("text");
         $text_edit.val(text);
-        $text_preview.html(marked(text));
     }
     if (l.getItem("title") != null) {
         $title.val(l.getItem("title"));
+    }
+
+    if ($text_edit.val() != null) {
+        $text_preview.html(marked($text_edit.val()));
     }
 
     // 変更をマークダウンにしプレビューに反映する
@@ -25,4 +30,10 @@ $(function() {
         l.setItem("title", $(this).val());
     });
 
+    $submit_btn.click(function() {
+        // TODO: validation check
+        console.log("checked");
+        l.removeItem("text");
+        l.removeItem("title");
+    });
 });
