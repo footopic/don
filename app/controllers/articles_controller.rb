cc42f8f87c74e689fdd4aa61b06065e3f183a3da
elzzup <guild0105@gmail.com>
2015-11-23 15:08:59 +0900

    Create bootstrap alert

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.html { redirect_to @article, flash: { success: 'Article was successfully created.'} }
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+        format.html { redirect_to @article, flash: { success: 'Article was successfully updated.'} }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to articles_url, flash: { success: 'Article was successfully deleted.'} }
       format.json { head :no_content }
     end
   end
