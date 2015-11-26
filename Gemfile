5a9f86a0f6347f5f49fd1cccc327874b76f71cd2
elzzup <guild0105@gmail.com>
2015-11-26 23:23:13 +0900

    Create article title preview in editor

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'autoprefixer-rails'
 
 gem 'font-awesome-rails'
 
+gem 'seed-fu'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/assets/javascripts/article_form.js
+++ app/assets/javascripts/article_form.js
@@ -1,6 +1,8 @@
 $(function() {
     var $title = $('#article_title');
     var $text_edit = $('#article_text');
+    var $categories_preview = $('#categories-preview');
+    var $title_preview = $('#title-preview');
     var $text_preview = $('#text-preview-area');
     var $submit_btn = $('#btn-submit');
     if (!$title.length || !$text_edit.length) {
@@ -9,6 +11,7 @@ $(function() {
     var l = localStorage;
 
     function updatePreview() {
+        // NOTE: marked で xss escape 済みで返る
         $text_preview.html(marked($text_edit.val()));
         // code hilighting
         $('pre code').each(function(i, block) {
@@ -17,11 +20,23 @@ $(function() {
         // 絵文字
         emojify.run($text_preview[0]);
     }
+    function updateTitlePreview() {
+        var words = $title.val().split('/');
+        var title = words.pop();
+        var categories_text = words.join('/');
+        $title_preview.text(title);
+        $categories_preview.text(categories_text);
+        // 絵文字
+        emojify.run($title_preview[0]);
+    }
 
     console.log($text_edit.val());
     console.log($text_edit.val() != null);
     if ($text_edit.val() != null) {
         updatePreview();
+    }
+    if ($title.val() != null) {
+        updateTitlePreview();
     }
 
     // // ローカルストレージに保存されていたら復元
@@ -40,6 +55,7 @@ $(function() {
     });
     $title.keyup(function() {
         l.setItem("title", $(this).val());
+        updateTitlePreview();
     });
 
     $submit_btn.click(function() {
