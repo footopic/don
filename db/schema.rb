0ea8f9ab59dddd419efd141dfbbd7cc8bc649f5f
akameco <akameco.t@gmail.com>
2015-11-27 16:49:34 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105214640) do
+ActiveRecord::Schema.define(version: 20151125151630) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,10 +22,8 @@ ActiveRecord::Schema.define(version: 20151105214640) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string  "provider", default: "", null: false
+    t.integer "uid",                   null: false
   end
 
 end
