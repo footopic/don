efca3671bb031a2f451980fc5603db5628b1f8f6
akameco <akameco.t@gmail.com>
2015-11-29 02:41:26 +0900

    Update article schema

--- db/migrate/20151105214640_create_articles.rb
+++ db/migrate/20151105214640_create_articles.rb
@@ -1,11 +1,13 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string :title
-      t.string :text
-      t.integer :user_id
+      t.string :title, null: false
+      t.text :text, null: false
+      t.references :user, index: true, null: false, foreign_key: true
 
       t.timestamps null: false
     end
+
+    add_index :articles, :title
   end
 end
