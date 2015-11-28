5b175b77fb86c0573452a72e529fbd260574a34e
akameco <akameco.t@gmail.com>
2015-11-29 02:20:57 +0900

    Remove not using action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,66 +1,28 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
   # POST /comments
-  # POST /comments.json
   def create
 
     @comment = Comment.new(comment_params)
     @article = @comment.article
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to article_path(@article), notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to article_path(@article), notice: 'コメントを投稿しました'
     end
   end
 
   # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update(comment_params)
+      redirect_to @comment, notice: 'コメントを更新しました'
     end
   end
 
   # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to article_path(@comment.article), notice: 'コメントを削除しました'
   end
 
   private
