64e52843d938eb51961a23ca7e5a778113aeffcf
elzzup <guild0105@gmail.com>
2015-12-07 01:25:11 +0900

    Fix unicorn file

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,7 +4,7 @@ working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
 # listen
-listen "#{app_path}/tmp/sockets/unicorn.footopic.socket", :backlog => 64
+listen "#{app_path}/current/tmp/sockets/unicorn.footopic.socket", :backlog => 64
 
 # logging
 stderr_path "log/unicorn.stderr.log"
