2919454a3ceddbacea5105647984989c34531fbd
akameco <akameco.t@gmail.com>
2015-12-09 22:30:20 +0900

    Add secrets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+AdminUser.destroy_all
+
+admin = Rails.application.secrets.admin
+AdminUser.create!(email: admin['email'], password: admin['password'], password_confirmation: admin['password'])
