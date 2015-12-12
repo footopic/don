7f31bb65f509db4ae67ad5fc2daccc50cbffb997
akameco <akameco.t@gmail.com>
2015-12-13 04:59:54 +0900

    Update ログインしている場合のサイドバー

--- spec/features/sidebars_spec.rb
+++ spec/features/sidebars_spec.rb
@@ -1,18 +1,45 @@
 require 'rails_helper'
 
-RSpec.feature "Sidebars", type: :feature do
+RSpec.feature "サイドバー", type: :feature do
   before do
     visit root_path
   end
 
   context 'ログインしている場合' do
-    context 'WRITEボタンをクリックしたとき' do
+    let(:user) { create(:user) }
+
+    before do
+      login(user)
     end
 
     context 'ARTICLESボタンをクリックしたとき' do
+      before do
+        find('#side-bar__articles').click
+      end
+
+      it '記事一覧を表示' do
+        expect(current_path).to eq articles_path
+      end
+    end
+
+    context 'WRITEボタンをクリックしたとき' do
+      before do
+        find('#side-bar__write').click
+      end
+
+      it '記事作成ページに遷移すること' do
+        expect(current_path).to eq new_article_path
+      end
     end
 
     context 'MEMBERSボタンをクリックしたとき' do
+      before do
+        find('#side-bar__members').click
+      end
+
+      it 'ユーザ一覧ページに遷移すること' do
+        expect(current_path).to eq users_path
+      end
     end
   end
 
