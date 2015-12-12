ca2a6930af70e7f2488f46b4493872f71f2b3729
akameco <akameco.t@gmail.com>
2015-12-13 02:24:40 +0900

    Update features spec ログインメッセージ

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -12,12 +12,22 @@ RSpec.describe 'トップページ', type: :feather do
       visit root_path
       expect(page).not_to have_content('ログイン')
     end
+
+    it 'ログインしてくださいとメッセージが表示されないこと' do
+      visit users_path
+      expect(page).not_to have_content('ログインまたは登録が必要です')
+    end
   end
 
   context 'ログインしていない場合' do
     it 'ログインリンクが表示されていること' do
       visit root_path
       expect(page).to have_content('ログイン')
+    end
+
+    it 'ログインしてくださいとメッセージが表示されること' do
+      visit users_path
+      expect(page).to have_content('ログインまたは登録が必要です')
     end
   end
 end
