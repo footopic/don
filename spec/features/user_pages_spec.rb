2808257fd69933b2c9a0cd38152246427c8e3ed1
akameco <akameco.t@gmail.com>
2015-12-13 14:26:38 +0900

    Add spec with other user

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -19,5 +19,31 @@ RSpec.feature 'ユーザページ', type: :feature do
         expect(page).to have_content(user.name)
       end
     end
+
+    context '別のユーザがいるとき' do
+      let!(:other) { create(:other_user) }
+      before do
+        visit users_path
+      end
+
+      it '他のユーザの名前があること' do
+        expect(page).to have_content(other.name)
+      end
+
+      it 'リスト要素に2つあること' do
+        expect(page).to have_content(other.name)
+      end
+
+      context '他のユーザが退会したとき' do
+        before do
+          other.destroy
+          visit users_path
+        end
+
+        it '他のユーザの名前がないこと' do
+          expect(page).not_to have_content(other.name)
+        end
+      end
+    end
   end
 end
