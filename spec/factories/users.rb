64fcb099184b7ed92e5a60ae4831fcd653646e4e
akameco <akameco.t@gmail.com>
2015-12-13 14:26:20 +0900

    Add other_user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,11 +18,28 @@
 #
 
 FactoryGirl.define do
+
+  sequence :name do
+    FFaker::Name.name
+  end
+
+  sequence :screen_name do
+    FFaker::Internet.user_name
+  end
+
   factory :user do
     uid '12345678'
     provider 'google_oauth2'
     screen_name 'eri'
     name 'エリザベート・バートリー'
+    image File.open(File.join(Rails.root, 'spec/factories/sample.jpg'))
+  end
+
+  factory :other_user, class: User do
+    sequence(:uid) { |n| "#{n * 10}" }
+    provider 'google_oauth2'
+    screen_name
+    name
     image File.open(File.join(Rails.root, 'spec/factories/sample.jpg'))
   end
 end
