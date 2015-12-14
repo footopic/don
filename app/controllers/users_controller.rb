6f200fc1b734e2ef3ecb0d0854388679e57af5f1
akameco <akameco.t@gmail.com>
2015-12-15 01:29:32 +0900

    Update controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,11 +66,11 @@ class UsersController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find_by(screen_name: params[:id])
   end
 
   def current_user_check
-    unless current_user.equal_id? @user
+    unless current_user == @user
       respond_to do |format|
         format.html { redirect_to @user, notice: '編集権限がありません' }
       end
