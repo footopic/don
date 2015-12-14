32ce4b59c9b769993f6fce40399d9904f254c31b
akameco <akameco.t@gmail.com>
2015-12-15 04:07:46 +0900

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,13 @@ module ApplicationHelper
       end
     end
   end
+
+  def full_title(page_title)
+    base_title = '丼'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
 end
