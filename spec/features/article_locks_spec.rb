3e1a5927ab3cf394e13be2e101d9a2793258d69f
akameco <akameco.t@gmail.com>
2015-12-16 03:21:55 +0900

    Add test

--- spec/features/article_locks_spec.rb
+++ spec/features/article_locks_spec.rb
@@ -1,24 +1,65 @@
 require 'rails_helper'
 
-RSpec.feature '記事のロック', type: :feature do
+RSpec.feature '記事の保護', type: :feature do
   context '自分の記事' do
     let!(:user) { create(:user) }
-    let!(:article) { create(:article, user: user, lock: true) }
+    context '保護されているとき' do
+      let!(:article) { create(:article, user: user, lock: true) }
 
-    before do
-      login(user)
-      visit article_path(article)
+      before do
+        login(user)
+        visit article_path(article)
+      end
+
+      it '編集ボタンが表示されること' do
+        within('.article-titles') do
+          expect(page).to have_css('.fa-pencil')
+        end
+      end
+
+      it { expect(page).to have_link('記事の保護を解除する') }
+
+      context '解除リンクをクリックしたとき' do
+        before do
+          click_link '記事の保護を解除する'
+        end
+
+        it { expect(page).to have_content('保護を解除しました') }
+        it { expect(page).to have_link('記事を保護する') }
+      end
     end
 
-    it '編集ボタンが表示されること' do
-      within('.article-titles') do
-        expect(page).to have_css('.fa-pencil')
+    context '保護されていないとき' do
+      let!(:article) { create(:article, user: user, lock: false) }
+
+      before do
+        login(user)
+        visit article_path(article)
+      end
+
+      it '編集ボタンが表示されること' do
+        within('.article-titles') do
+          expect(page).to have_css('.fa-pencil')
+        end
+      end
+
+      it { expect(page).to have_link('記事を保護する') }
+
+      context '保護リンクをクリックしたとき' do
+        subject { page }
+        before do
+          click_link '記事を保護する'
+        end
+
+        it { is_expected.to have_content('保護しました') }
+        it { is_expected.to have_link('記事の保護を解除する') }
+        it { is_expected.to have_css('.fa-lock') }
       end
     end
   end
 
   context '他人の記事' do
-    context 'ロックされていない場合' do
+    context '保護されていない場合' do
       let!(:user) { create(:user) }
       let!(:other) { create(:user) }
       let!(:article) { create(:article, user: other, lock: false) }
@@ -42,7 +83,7 @@ RSpec.feature '記事のロック', type: :feature do
       end
     end
 
-    context 'ロックされている場合' do
+    context '保護されている場合' do
       let!(:user) { create(:user) }
       let!(:other) { create(:user) }
       let!(:article) { create(:article, user: other, lock: true) }
@@ -56,6 +97,10 @@ RSpec.feature '記事のロック', type: :feature do
         within('.article-titles') do
           expect(page).not_to have_css('.fa-pencil')
         end
+      end
+
+      it '編集ボタンが表示されないこと' do
+          expect(page).to have_css('.fa-lock')
       end
     end
   end
