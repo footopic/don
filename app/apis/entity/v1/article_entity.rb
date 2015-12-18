c9c814ed925b79b0d68059a6174a31e24499f905
elzzup <guild0105@gmail.com>
2015-12-19 02:37:40 +0900

    Fix n+1 users api

--- app/apis/entity/v1/article_entity.rb
+++ app/apis/entity/v1/article_entity.rb
@@ -4,7 +4,7 @@ module Entity
       expose :title
       expose :text
       expose :user, using: UserSimpleEntity
-      expose :tags
+      expose :tag_names, as: :tags
       expose :history_count do |a| a.histories.size end
       expose :comment_count do |a| a.comments.size end
       expose :star_count do |a| a.stars.size end
