020443001fd6e28bfff757fc455690e3761e7d02
elzzup <guild0105@gmail.com>
2015-12-18 23:56:52 +0900

    Fix markdown render

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,7 +2,13 @@ module MarkdownHelper
   def markdown(text)
     unless @markdown
       renderer  = Redcarpet::Render::HTML.new(hard_wrap: true)
-      @markdown = Redcarpet::Markdown.new(renderer, tables: true)
+      @markdown = Redcarpet::Markdown.new(
+          renderer,
+          tables: true,
+          strikethrought: true,
+          fenced_code_blocks: true,
+          space_after_headers: true
+      )
     end
 
     @markdown.render(text).html_safe
