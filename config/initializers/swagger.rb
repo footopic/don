b863b409c4a877b9b8f398a3c17a630b16b66a42
elzzup <guild0105@gmail.com>
2015-12-20 21:09:05 +0900

    Api basic auth

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,3 +1,7 @@
 GrapeSwaggerRails.options.url      = 'swagger_doc'
 GrapeSwaggerRails.options.app_name = 'Don'
 GrapeSwaggerRails.options.app_url  = '/api/v1/'
+
+GrapeSwaggerRails.options.api_auth     = 'basic' # Or 'bearer' for OAuth
+GrapeSwaggerRails.options.api_key_name = 'Authorization'
+GrapeSwaggerRails.options.api_key_type = 'header'
